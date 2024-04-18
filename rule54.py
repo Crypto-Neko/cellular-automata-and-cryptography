cda540e6ea919d659e717a84bb4306c77def9057
Crypto-Neko <objective.axioma@gmail.com>
2024-04-18 15:42:38 -0400

    Added a new example.

--- rule54.py
+++ rule54.py
@@ -41,6 +41,5 @@ class Automata():
 
 
 
-tiles = []
-tiles.append((15, 1))
-automata = Automata(31, tiles)
+tiles = [(1, 1), (3, 1), (7, 1), (15, 1), (11, 1), (12, 1), (13, 1), (5, 1), (12, 1)]
+automata = Automata(20, tiles)
